f70cbd6179d83d5bc19b3b2e5eea738f8d6eb21a
phantom05 <monster2jy@gmail.com>
2019-11-27 00:25:57 +0900

    next  redux 적용해보기

--- next-study-st1/pages/about.js
+++ next-study-st1/pages/about.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import {PlainTemplate} from 'components/base/template';
-import {MainHeader} from 'components/base/header';
+import { PlainTemplate } from 'components/base/template';
+import { MainHeader } from 'components/base/header';
+import { getUser } from 'lib/api'
 
 class About extends Component {
+  static async getInitialProps() {
+    const { data } = await getUser();
+    return {
+      data: data
+    }
+  }
   render() {
+    console.log(this.props);
+    const { data } = this.props;
     return (
       <PlainTemplate header={<MainHeader />}>
         About
+        <ul>
+          {data.map(
+            user => <li key={user.id}>{user.name}</li>
+          )}
+        </ul>
       </PlainTemplate>
     );
   }
 }
+
 
 export default About;
